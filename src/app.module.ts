9c84728b048fcd75ab271b162c3bc35cefa95ab7
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-05-01 13:31:53 +0300

    configuration and cachemodule setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,32 @@
 import { Module } from '@nestjs/common'
 import { PrismaModule } from './prisma/prisma.module'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { redisStore } from 'cache-manager-redis-yet'
+import { CacheModule } from '@nestjs/cache-manager'
+import app from './config/app'
+import redis from './config/redis'
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      load: [app, redis],
+      isGlobal: true,
+    }),
+    CacheModule.registerAsync({
+      useFactory: async (config) => {
+        const store = await redisStore({
+          ttl: 10 * 1000,
+          socket: {
+            host: config.get('redis.host'),
+            port: config.get('redis.port'),
+          },
+        })
+        return { store }
+      },
+      inject: [ConfigService],
+      isGlobal: true,
+    }),
+    PrismaModule,
+  ],
 })
 export class AppModule {}
