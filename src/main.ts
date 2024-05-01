924b5e3377929d6cf2a5c5d35fd636902ac306e9
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-05-01 13:33:40 +0300

    swagger setup

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,19 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ConfigService } from '@nestjs/config'
 import { ValidationPipe } from '@nestjs/common'
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const config = app.get(ConfigService)
   app.useGlobalPipes(new ValidationPipe())
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Messenger app')
+    .setDescription('The messenger API description')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  SwaggerModule.setup('api', app, document)
   await app.listen(config.get('port'))
 }
 
