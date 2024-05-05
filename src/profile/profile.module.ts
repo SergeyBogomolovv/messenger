a2bd9960f7e514143dfce4057d7e9d15fd5d51f3
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-05-05 21:39:32 +0300

    add update user info

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
 import { ProfileService } from './profile.service'
 import { ProfileController } from './profile.controller'
+import { AwsModule } from 'src/aws/aws.module'
 
 @Module({
+  imports: [AwsModule],
   providers: [ProfileService],
   controllers: [ProfileController],
 })
