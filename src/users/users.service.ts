30810347349282973cd75e7ff63c0b436faa850d
Sergey Bogomolov <bogomolovs693@gmail.com>
2024-05-06 18:45:45 +0300

    changed check is username exists logic and add normal caching

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -23,20 +23,35 @@ export class UsersService {
     await this.cache.set(newUser.id, newUser)
     return newUser
   }
-  async findOne(idOrEmail: string) {
-    const cachedUser: User = await this.cache.get(idOrEmail)
+
+  async findByUsername(username: string, existingUsername?: string) {
+    const existingUser = await this.findOne(username)
+    if (existingUser) {
+      if (existingUsername) {
+        if (existingUser.username === existingUsername) {
+          return false
+        }
+      }
+      return true
+    }
+    return false
+  }
+
+  async findOne(slug: string) {
+    const cachedUser: User = await this.cache.get(slug)
     if (!cachedUser) {
       const dbUser = await this.prisma.user.findFirst({
         where: {
-          OR: [{ id: idOrEmail }, { email: idOrEmail }],
+          OR: [{ id: slug }, { email: slug }, { username: slug }],
         },
       })
       if (!dbUser) return null
-      await this.cache.set(idOrEmail, dbUser)
+      await this.cache.set(slug, dbUser)
       return dbUser
     }
     return cachedUser
   }
+
   async delete(id: string) {
     await this.cache.del(id)
     return this.prisma.user.delete({ where: { id } })
